aa579f00a752ab1f169e1e47c0d5f4968d8f9191
sebamateos <sebitamateoss1080@gmail.com>
2024-03-13 13:37:14 -0300

    agregue variables de entorno

--- tienda-stay/src/components/layout/Layout.jsx
+++ tienda-stay/src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./navBar/NavBar";
-import { Footer } from "./Footer/Footer";
+import { Footer } from "./footer/Footer";
 
 export const Layout = () => {
   return (
